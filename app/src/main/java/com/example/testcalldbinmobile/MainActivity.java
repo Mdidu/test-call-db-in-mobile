40e6256938b3a99eb906de0e56d612af64cab595
Alexandre Meddas <alexandre.meddas@gmail.com>
2020-12-22 10:42:26 +0100

    Call db in mobile V1

--- app/src/main/java/com/example/testcalldbinmobile/MainActivity.java
+++ app/src/main/java/com/example/testcalldbinmobile/MainActivity.java
@@ -8,18 +8,6 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
-import java.util.Iterator;
-
 public class MainActivity extends AppCompatActivity {
 
     private static TextView txt = null;
